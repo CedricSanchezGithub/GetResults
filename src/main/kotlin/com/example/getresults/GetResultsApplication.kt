81948d10273ef02ef569186414cda08d7bf72667
CedricSanchezGithub <mycedmail@gmail.com>
2024-09-05 13:26:17 +0200

    first commit

---
+++ src/main/kotlin/com/example/getresults/GetResultsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.getresults
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GetResultsApplication
+
+fun main(args: Array<String>) {
+    runApplication<GetResultsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/getresults/GetResultsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.getresults
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GetResultsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
